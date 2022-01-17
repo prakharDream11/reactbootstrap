ec54c395735efced4d61c51a591d507bd3dd7b7e
Prakhar Dixit <prakhar.dixit@dream11.com>
2022-01-17 13:21:26 +0530

    performance

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Spinner from 'react-bootstrap/Spinner';
+import {InputGroup, FormControl} from 'react-bootstrap';
+import Pagination from 'react-bootstrap/Pagination';
+
+
+
+
 
 function App() {
+  let active = 2;
+let items = [];
+for (let number = 1; number <= 5; number++) {
+  items.push(
+    <Pagination.Item key={number} active={number === active}>
+      {number}
+    </Pagination.Item>,
+  );
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Alert key={0} variant='primary'>
+    This is a {'primary'} alert—check it out!
+  </Alert>
+  <Spinner animation='border' />
+  <InputGroup size="sm" className="mb-3">
+    <InputGroup.Text id="inputGroup-sizing-sm">Small</InputGroup.Text>
+    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+  </InputGroup>
+  <Pagination size="lg">{items}</Pagination>
+
+    </>
   );
 }
 
